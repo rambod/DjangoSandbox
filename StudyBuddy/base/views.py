9d064ad738eef9ef0a9417c9c4a2194a3e1c2d53
Rambod <gh.rambod@gmail.com>
2023-07-31 19:43:53 +0300

    "Implement Room CRUD Functionality with Forms"
    Description: This commit introduces comprehensive CRUD (Create, Read, Update, Delete) functionality for the Room model in our Django project. Users can now effortlessly manage rooms through a user-friendly interface.
    
    The following changes have been made:
    
    Created views, templates, and URL patterns for creating, updating, deleting, and displaying rooms.
    Implemented Django forms to handle user input for room creation and updates, ensuring data integrity and ease of use.
    Associated appropriate model methods to handle room creation, updates, and deletions.
    Added validation and error handling to guarantee that user-submitted data adheres to specified constraints.
    With this commit, users can now effortlessly create, modify, and delete rooms, enhancing the interactivity and versatility of the StudyBuddy application.

--- StudyBuddy/base/views.py
+++ StudyBuddy/base/views.py
@@ -1,31 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
-rooms = [
-    {
-        'id': 1,
-        'name': 'Learning Vue js'
-    },
-    {
-        'id': 2,
-        'name': 'Learning React js'
-    },
-    {
-        'id': 3,
-        'name': 'Learning Angular js'
-    }
-]
+# rooms = [
+#     {
+#         'id': 1,
+#         'name': 'Learning Vue js'
+#     },
+#     {
+#         'id': 2,
+#         'name': 'Learning React js'
+#     },
+#     {
+#         'id': 3,
+#         'name': 'Learning Angular js'
+#     }
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = { 'rooms': rooms }
     return render(request,'base/home.html',context=context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == pk:
-            room = i
+    room = Room.objects.get(id=pk)
     context = { 'room' : room}
             
     return render(request,'base/room.html',context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form' : form}
+    return render(request,'base/room_form.html',context)
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form' : form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':room})
