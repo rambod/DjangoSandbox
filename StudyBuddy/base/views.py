698a970fe805dd4d64ea20b87c4434cf29ac69bd
Rambod <gh.rambod@gmail.com>
2023-08-02 17:19:32 +0300

    "Implement Recent Activity and Refactor Components for Reusability"
    Description: This commit introduces exciting improvements to the StudyBuddy app, enhancing the user experience and making the codebase more maintainable:
    
    Recent Activity on Home Page:
    
    Added a "Recent Activity" section on the home page, displaying the latest messages alongside feeds.
    Users can now effortlessly stay informed about the latest discussions and updates within the StudyBuddy community.
    Component Refactoring for Reusability:
    
    Broke down larger components (e.g., feeds, activity, topics) into smaller, reusable chunks.
    Created independent component files that can be included using {% include %} tags, promoting code modularity and maintainability.
    This refactoring facilitates easier code management, encourages code reuse, and streamlines future updates and improvements.
    With these changes, StudyBuddy becomes more dynamic, providing users with up-to-date information through the "Recent Activity" feature. Additionally, the component refactoring enhances code organization and reusability, laying the groundwork for a scalable and maintainable application.

--- StudyBuddy/base/views.py
+++ StudyBuddy/base/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render,redirect ,HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from .models import Room, Topic, Message
-from .forms import RoomForm ,MessageForm
+from .forms import RoomForm, MessageForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
@@ -24,34 +24,38 @@ from django.contrib import messages
 #     }
 # ]
 
+
 def loginPage(request):
     page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
-    
+
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
-        
+
         try:
             user = User.objects.get(username=username)
         except:
-            messages.add_message(request, messages.ERROR, 'User does not exist')
-            
+            messages.add_message(request, messages.ERROR,
+                                 'User does not exist')
+
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            messages.add_message(request, messages.ERROR, 'Username or password is incorrect')
-            
-    context = {'page':page}
-    return render(request,'base/login_register.html',context)
+            messages.add_message(request, messages.ERROR,
+                                 'Username or password is incorrect')
+
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
 
 def registerPage(request):
     page = "register"
@@ -65,39 +69,46 @@ def registerPage(request):
             login(request, user)
             return redirect('home')
         else:
-            messages.add_message(request, messages.ERROR, 'An error occurred during registration')
-    
-    return render(request, 'base/login_register.html', {'page':page, 'form':form})
+            messages.add_message(request, messages.ERROR,
+                                 'An error occurred during registration')
+
+    return render(request, 'base/login_register.html', {'page': page, 'form': form})
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q)| Q(description__icontains=q))
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(
+        name__icontains=q) | Q(description__icontains=q))
     topics = Topic.objects.all()
-    
-    room_count = rooms.count()
-        
-    context = { 'rooms': rooms , 'topics': topics,'room_count':room_count}
-    return render(request,'base/home.html',context=context)
 
-def room(request,pk):
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q)).order_by('-created')[:5]
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count,'room_messages':room_messages}
+    return render(request, 'base/home.html', context=context)
+
+
+def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
     participents = room.participants.all()
-    
+
     if request.method == "POST":
         message = Message.objects.create(
-            user = request.user,
-            room = room,
-            body = request.POST.get('body')
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
         )
         room.participants.add(request.user)
         room.save()
         message.save()
         return redirect('room', pk=room.id)
     print(participents)
-    context = { 'room' : room,'room_messages':room_messages,'participants':participents}
-            
-    return render(request,'base/room.html',context)
+    context = {'room': room, 'room_messages': room_messages,
+               'participants': participents}
+
+    return render(request, 'base/room.html', context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
@@ -107,58 +118,61 @@ def createRoom(request):
         if form.is_valid():
             form.save()
             return redirect('home')
-    context = {'form' : form}
-    return render(request,'base/room_form.html',context)
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
 
 @login_required(login_url='login')
-def updateRoom(request,pk):
+def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    
+
     if request.user != room.host:
         return HttpResponse("you are not allowed here")
     if request.method == 'POST':
-        form = RoomForm(request.POST,instance=room)
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
             return redirect('home')
-    context = {'form' : form}
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+
 @login_required(login_url='login')
-def deleteRoom(request,pk):
+def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
-    
+
     if request.user != room.host:
         return HttpResponse("you are not allowed here")
     if request.method == 'POST':
         room.delete()
         return redirect('home')
-    return render(request,'base/delete.html',{'obj':room})
+    return render(request, 'base/delete.html', {'obj': room})
 
 
 @login_required(login_url='login')
-def deleteMessage(request,pk):
+def deleteMessage(request, pk):
     message = Message.objects.get(id=pk)
-    
+
     if request.user != message.user:
         return HttpResponse("you are not allowed here")
     if request.method == 'POST':
         message.delete()
         return redirect('home')
-    return render(request,'base/delete.html',{'obj':message})
+    return render(request, 'base/delete.html', {'obj': message})
+
 
 @login_required(login_url='login')
-def updateMessage(request,pk):
+def updateMessage(request, pk):
     message = Message.objects.get(id=pk)
     form = MessageForm(instance=message)
-    
+
     if request.user != message.user:
         return HttpResponse("you are not allowed here")
     if request.method == 'POST':
-        form = MessageForm(request.POST,instance=message)
+        form = MessageForm(request.POST, instance=message)
         if form.is_valid():
             form.save()
             return redirect('home')
-    context = {'form' : form}
+    context = {'form': form}
     return render(request, 'base/message_form.html', context)
