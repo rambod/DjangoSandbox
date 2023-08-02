46b2859d3b498513b0c4ab3c1d51ff2f3f6eb911
Rambod <gh.rambod@gmail.com>
2023-08-02 13:52:23 +0300

    "Implement User Authentication and Flash Messages"
    Description: This commit marks a significant milestone in the project, as it introduces essential user authentication features and enhances user feedback:
    
    User Authentication:
    
    Added login and logout functionality, enabling users to securely access their accounts.
    Implemented authentication checks for certain views, ensuring that only authenticated users can access sensitive content.
    Flash Messages:
    
    Integrated simple flash messages to provide immediate feedback to users when errors occur during form submissions or other actions.
    These flash messages offer informative cues for users, enhancing the overall user experience.
    With user authentication now in place, the project moves closer to full-fledged user registration and a more personalized experience. The addition of flash messages improves user interactions, making the application more intuitive and user-friendly. The upcoming next step will focus on user registration, further enhancing the StudyBuddy app's functionality and usability.

--- StudyBuddy/base/views.py
+++ StudyBuddy/base/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect ,HttpResponse
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 # rooms = [
@@ -19,6 +23,33 @@ from .forms import RoomForm
 #     }
 # ]
 
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.add_message(request, messages.ERROR, 'User does not exist')
+            
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.add_message(request, messages.ERROR, 'Username or password is incorrect')
+            
+    context = {}
+    return render(request,'base/login_register.html',context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -26,6 +57,7 @@ def home(request):
     topics = Topic.objects.all()
     
     room_count = rooms.count()
+        
     context = { 'rooms': rooms , 'topics': topics,'room_count':room_count}
     return render(request,'base/home.html',context=context)
 
@@ -35,7 +67,7 @@ def room(request,pk):
             
     return render(request,'base/room.html',context)
 
-
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -46,9 +78,13 @@ def createRoom(request):
     context = {'form' : form}
     return render(request,'base/room_form.html',context)
 
+@login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    
+    if request.user != room.host:
+        return HttpResponse("you are not allowed here")
     if request.method == 'POST':
         form = RoomForm(request.POST,instance=room)
         if form.is_valid():
@@ -57,8 +93,12 @@ def updateRoom(request,pk):
     context = {'form' : form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url='login')
 def deleteRoom(request,pk):
     room = Room.objects.get(id=pk)
+    
+    if request.user != room.host:
+        return HttpResponse("you are not allowed here")
     if request.method == 'POST':
         room.delete()
         return redirect('home')
