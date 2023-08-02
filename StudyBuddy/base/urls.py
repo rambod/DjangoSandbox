7b779be4d0de54de3cefae70ed9d3598c1917c69
Rambod <gh.rambod@gmail.com>
2023-08-02 17:40:22 +0300

    "Enhance User Profiles: Introduce User-Specific Profile Pages and Post History"
    Description: This commit represents a significant advancement in the StudyBuddy app, as it introduces comprehensive user profile features:
    
    User-Specific Profile Pages:
    
    Created dedicated profile pages for each user, accessible through their unique URLs.
    Users can now view their own profile, as well as profiles of other users, fostering a sense of community and connection.
    Post History Display:
    
    Enabled users to access their post history on their profile page, showcasing their contributions to the StudyBuddy platform.
    Users can also view the post history of other users, providing insight into their interactions and contributions.
    These new profile features enrich the StudyBuddy experience, allowing users to explore and engage with the platform in a more personalized and meaningful way. The addition of user-specific profiles and post history fosters a sense of belonging within the StudyBuddy community and encourages active participation in discussions and knowledge sharing.

--- StudyBuddy/base/urls.py
+++ StudyBuddy/base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register/',views.registerPage, name = "register"),
     path('logout/', views.logoutUser, name = "logout"),
     path('', views.home, name = "home"),
+    path('profile/<int:pk>', views.userPofile, name = "user-profile"),
     path('room/<int:pk>/', views.room, name = "room"),
     path('create-room/', views.createRoom, name = "create-room"),
     path('update-room/<int:pk>/', views.updateRoom, name = "update-room"),
--- StudyBuddy/base/views.py
+++ StudyBuddy/base/views.py
@@ -88,6 +88,19 @@ def home(request):
     return render(request, 'base/home.html', context=context)
 
 
+def userPofile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context ={
+        'user': user,
+        'rooms': rooms,
+        'room_messages': room_messages,
+        'topics': topics
+    }
+    return render(request, 'base/profile.html',context)
+
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
