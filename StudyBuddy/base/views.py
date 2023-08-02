c69d0446d6f7a74dfa4801a9939e991ad9163f3b
Rambod <gh.rambod@gmail.com>
2023-08-02 17:59:18 +0300

    "Implement Static Files Setup and Resolve Create Room Bugs"
    Description: This commit introduces two vital improvements to the StudyBuddy app, enhancing overall functionality and user experience:
    
    Static Files Setup:
    
    Created a dedicated "static" folder to manage static assets (e.g., CSS, JavaScript, images).
    Configured Django to serve static files during development, ensuring proper rendering and styling of the application.
    Create Room Bug Fixes:
    
    Addressed and resolved bugs in the "Create Room" feature, ensuring smoother and more reliable room creation.
    Modified the functionality to automatically compute and assign participants based on user inputs, removing the need for manual input.
    Users no longer need to specify participants explicitly; the system handles it efficiently.
    With the static files setup in place, the StudyBuddy app achieves a more consistent and polished appearance across various pages. Additionally, the Create Room bug fixes streamline the room creation process, enhancing user convenience and reducing the potential for errors. These changes contribute to an improved and seamless StudyBuddy experience for all users.

--- StudyBuddy/base/views.py
+++ StudyBuddy/base/views.py
@@ -129,6 +129,8 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
+            room = form.save(commit=False)
+            room.host = request.user
             form.save()
             return redirect('home')
     context = {'form': form}
